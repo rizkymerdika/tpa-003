98a7d14f2b4401bba1e1208551a77dcac0e8121a
Rizky Merdika Agusta <rizkymerdika@gmail.com>
2022-10-12 20:59:02 +0700

    Ganti Bentuk Date

--- script.js
+++ script.js
@@ -20,6 +20,62 @@ function showMovies(data){
     main.innerHTML = ''
     data.forEach(movie => {
         const {title, poster_path, vote_average, release_date} = movie
+        let bulan = ''
+        let newDate = ''
+        function changeDate() {
+            for(let i = 5; i <= 6; i++){
+                bulan += release_date[i]
+            }
+            switch (bulan) {
+                case '01':
+                    bulan = 'Jan'
+                    break;
+                case '02':
+                    bulan = 'Feb'
+                    break;
+                case '03':
+                    bulan = 'Mar'
+                    break;
+                case '04':
+                    bulan = 'Apr'
+                    break;
+                case '05':
+                    bulan = 'May'
+                    break;
+                case '06':
+                    bulan = 'Jun'
+                    break;
+                case '07':
+                    bulan = 'Jul'
+                    break;
+                case '08':
+                    bulan = 'Aug'
+                    break;
+                case '09':
+                    bulan = 'Sep'
+                    break;
+                case '10':
+                    bulan = 'Oct'
+                    break;
+                case '11':
+                    bulan = 'Nov'
+                    break;
+                case '12':
+                    bulan = 'Dec'
+                    break;
+                default:
+                    break;
+            }
+            newDate += bulan + ' '
+            for(let i = 8; i<=9; i++){
+                newDate += release_date[i]
+            }
+            newDate += ',' + ' '
+            for(let i = 0; i<=3; i++){
+                newDate += release_date[i]
+            }
+            return newDate
+        }
         const movieElement = document.createElement('div')
         movieElement.classList.add('movie')
         movieElement.innerHTML = `
@@ -30,7 +86,7 @@ function showMovies(data){
                 <span class="${getColor(vote_average)}">${vote_average}</span>
             </div>
             <div class="date">
-                <span class="date-detail">${release_date}</span>
+                <span class="date-detail">${changeDate()}</span>
             </div>
         `
         main.appendChild(movieElement)
